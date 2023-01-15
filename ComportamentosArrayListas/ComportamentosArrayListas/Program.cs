a47a54798c2a0cbdd50231bbc0d94339bdc1864d
= <=>
2023-01-15 01:14:11 -0300

    Seção #6 em andamento

---
+++ ComportamentosArrayListas/ComportamentosArrayListas/Program.cs
@@ -0,0 +1,16 @@
+﻿using Memoria;
+
+namespace ComportamentosArrayListas
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Point p;
+			p.x = 10;
+			p.y = 20;
+
+			Console.WriteLine(p);
+		}
+	}
+}
---
+++ ComportamentosArrayListas/Nullable/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Nullable
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			double? x = null;
+			double? y = 20;
+			double a = x ?? 20;
+			double b= y ?? 20;
+
+			Console.WriteLine(x.GetValueOrDefault());
+			Console.WriteLine(y.GetValueOrDefault());
+
+			Console.WriteLine(x.HasValue);
+			Console.WriteLine(y.HasValue);
+
+			Console.WriteLine(a);
+			Console.WriteLine(b);
+		}
+	}
+}
